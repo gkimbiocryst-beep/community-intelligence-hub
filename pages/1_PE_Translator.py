c0af62dc200cded8b6f6790ccb79c0a8646af7b0
gkimbiocryst-beep <gkimbiocryst@gmail.com>
2026-07-07 13:53:38 -0400

    Update 1_PE_Translator.py

--- pages/1_PE_Translator.py
+++ pages/1_PE_Translator.py
@@ -1,1 +1,7 @@
+import streamlit as st
 
+st.title("Patient Engagement Translator")
+
+st.write(
+    "This page will translate social listening insights into unmet needs and recommended actions."
+)
